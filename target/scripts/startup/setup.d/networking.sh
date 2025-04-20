cbe020e87a16acd248d6bcc39e09ef56d03d6649
gat <gattytto@thugnerdz.com>
2025-04-20 19:33:52 +0000

    v6

--- target/scripts/startup/setup.d/networking.sh
+++ target/scripts/startup/setup.d/networking.sh
@@ -37,13 +37,17 @@ function _setup_docker_permit() {
     _log 'trace' "Adding ${NETWORK_TYPE} (${NETWORK}) to Postfix 'main.cf:mynetworks'"
     _adjust_mtime_for_postfix_maincf
     postconf "$(postconf | grep '^mynetworks =') ${NETWORK}"
-    [[ ${ENABLE_OPENDMARC} -eq 1 ]] && echo "${NETWORK}" >>/etc/opendmarc/ignore.hosts
-    [[ ${ENABLE_OPENDKIM} -eq 1 ]] && echo "${NETWORK}" >>/etc/opendkim/TrustedHosts
+#    [[ ${ENABLE_OPENDMARC} -eq 1 ]] && echo "${NETWORK}" >>/etc/opendmarc/ignore.hosts
+#    [[ ${ENABLE_OPENDKIM} -eq 1 ]] && echo "${NETWORK}" >>/etc/opendkim/TrustedHosts
   }
 
   case "${PERMIT_DOCKER}" in
     ( 'none' )
       __clear_postfix_mynetworks
+      ;;
+    
+    ( 'v6' )
+      __add_to_postfix_mynetworks 'v6' "${MY_NETWORKS}"
       ;;
 
     ( 'connected-networks' )
